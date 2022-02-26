856a23fd6e13bb8fdc2fa094c938a6598389ed9f
N0x45 <noxas5inn@gmail.com>
2022-02-26 15:29:44 +0100

    :hammer: config-server :hammer

---
+++ spring-cloud-ms-config-server/src/main/java/com/noxas/springcloudmsconfigserver/SpringCloudMsConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.noxas.springcloudmsconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudMsConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudMsConfigServerApplication.class, args);
+	}
+
+}
